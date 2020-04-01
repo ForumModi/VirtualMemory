af13966cb98a24f8b37cd4d17c0d1cdd5974063e
osc <ForumModi>
2020-04-01 14:07:01 -0400

    Basic Main Added

---
+++ main.c
@@ -0,0 +1,20 @@
+/* 
+Project2
+Operating Systems
+Forum Modi & AJ Viola
+4/1/2020
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h> 
+#include <wait.h>
+
+
+int main(int argc, char** argv) {
+
+    int address = atoi(argv[1]);
+
+}
